7d0a3c349ad3d6d0bddbb7a2ebf1030f5f195cf3
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2022-08-26 20:34:24 -0300

    [UPDATE] Up new data files

---
+++ assets/js/data_harmonicas.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: "Amigo Chico",
+    artist: "Alegria Cristã | Harmônicas RAK 2022",
+    cover: "imgs/logo_com_escrita.png",
+    file: "https://firebasestorage.googleapis.com/v0/b/portal-alegria-crista.appspot.com/o/musicas%2FRAK%2F2022%2FHarmonicas%2FAmigo-Chico.mp3?alt=media&token=34ed40c8-50bc-4082-9385-e7599ad691d6",
+  },
+  {
+    title: "Cantando a paz",
+    artist: "Alegria Cristã | Harmônicas RAK 2022",
+    cover: "imgs/logo_com_escrita.png",
+    file: "https://firebasestorage.googleapis.com/v0/b/portal-alegria-crista.appspot.com/o/musicas%2FRAK%2F2022%2FHarmonicas%2FCantando-a-paz.mp3?alt=media&token=7c55424f-d190-450d-8326-80afab5eca17",
+  },
+  {
+    title: "Concafrar",
+    artist: "Alegria Cristã | Harmônicas RAK 2022",
+    cover: "imgs/logo_com_escrita.png",
+    file: "https://firebasestorage.googleapis.com/v0/b/portal-alegria-crista.appspot.com/o/musicas%2FRAK%2F2022%2FHarmonicas%2FConcafrar.mp3?alt=media&token=a595506b-3da5-4f14-aa4f-33b49b685653",
+  },
+  {
+    title: "Concafras",
+    artist: "Alegria Cristã | Harmônicas RAK 2022",
+    cover: "imgs/logo_com_escrita.png",
+    file: "https://firebasestorage.googleapis.com/v0/b/portal-alegria-crista.appspot.com/o/musicas%2FRAK%2F2022%2FHarmonicas%2FConcafras.mp3?alt=media&token=d79e1131-ca95-49fa-9a91-b52ca4797adb",
+  },
+  {
+    title: "Hino a Maria",
+    artist: "Alegria Cristã | Harmônicas RAK 2022",
+    cover: "imgs/logo_com_escrita.png",
+    file: "https://firebasestorage.googleapis.com/v0/b/portal-alegria-crista.appspot.com/o/musicas%2FRAK%2F2022%2FHarmonicas%2FHino-a-Maria.mp3?alt=media&token=2982847f-443d-4c08-9bcc-8709149e2ebe",
+  },
+  {
+    title: "Hino à Euripedes Barsanulfo",
+    artist: "Alegria Cristã | Harmônicas RAK 2022",
+    cover: "imgs/logo_com_escrita.png",
+    file: "https://firebasestorage.googleapis.com/v0/b/portal-alegria-crista.appspot.com/o/musicas%2FRAK%2F2022%2FHarmonicas%2FHino-%C3%A0-Euripedes-Barsanulfo.mp3?alt=media&token=42b42e6e-2e40-44fc-9b1d-558986c1cfe3",
+  },
+  {
+    title: "Nymphu",
+    artist: "Alegria Cristã | Harmônicas RAK 2022",
+    cover: "imgs/logo_com_escrita.png",
+    file: "https://firebasestorage.googleapis.com/v0/b/portal-alegria-crista.appspot.com/o/musicas%2FRAK%2F2022%2FHarmonicas%2FNymphu.mp3?alt=media&token=6fc23c83-6615-418e-bcf2-c1d8c8fb9abd",
+  },
+  {
+    title: "Prece III",
+    artist: "Alegria Cristã | Harmônicas RAK 2022",
+    cover: "imgs/logo_com_escrita.png",
+    file: "https://firebasestorage.googleapis.com/v0/b/portal-alegria-crista.appspot.com/o/musicas%2FRAK%2F2022%2FHarmonicas%2FPrece-III.mp3?alt=media&token=fb31ea6a-cd86-4488-9e82-dd9a91b8b99e",
+  },
+  {
+    title: "Seguir com Paulo",
+    artist: "Alegria Cristã | Harmônicas RAK 2022",
+    cover: "imgs/logo_com_escrita.png",
+    file: "https://firebasestorage.googleapis.com/v0/b/portal-alegria-crista.appspot.com/o/musicas%2FRAK%2F2022%2FHarmonicas%2FSeguir-com-Paulo.mp3?alt=media&token=f0f720de-fa1e-498d-aec8-0a245d05df0a",
+  },
+  {
+    title: "Somos nós",
+    artist: "Alegria Cristã | Harmônicas RAK 2022",
+    cover: "imgs/logo_com_escrita.png",
+    file: "https://firebasestorage.googleapis.com/v0/b/portal-alegria-crista.appspot.com/o/musicas%2FRAK%2F2022%2FHarmonicas%2FSomos-nos.mp3?alt=media&token=d1dc073b-ae1d-4a3a-b177-9336cf7e50cf",
+  },
+];
